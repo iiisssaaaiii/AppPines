259b55bef37a04207fca530d78d0173cbeb58cad
leonardoruizp <leorp3101@gmail.com>
2025-10-22 15:30:54 -0600

    se agregaron funciones de impresión y exportacion de pines en tamaño pequeño y grande

---
+++ frontend/src/components/pines/ConfirmationModal.jsx
@@ -0,0 +1,21 @@
+// src/components/pines/ConfirmationModal.jsx
+import React from 'react';
+import './ConfirmationModal.css';
+
+const ConfirmationModal = ({ isOpen, onClose, onConfirm, message }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <p>{message}</p>
+        <div className="modal-actions">
+          <button onClick={onConfirm} className="btn-confirm">Aceptar</button>
+          <button onClick={onClose} className="btn-cancel">Cancelar</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmationModal;
